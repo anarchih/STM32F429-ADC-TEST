13da3c11e6b2b6ae281a4beb0ac4da24457a849e
sonicyang <sonic.tw.tp@gmail.com>
2014-11-22 16:54:02 +0800

    Ficking working TIM6 TRGO

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_adc.h"
 #include "stm32f4xx_gpio.h"
+#include "stm32f4xx_tim.h"
+#include "stm32f4xx_dac.h"
 #define ADC1_DR_ADDRESS 0x4001204C
 uint8_t value_s;
 uint8_t value[1024];
 
 int main(){
 
-    ADC_InitTypeDef ADC_InitStructure;
-    ADC_CommonInitTypeDef ADC_CommonInitStructure;
+    ADC_InitTypeDef ADC_InitStruct;
+    ADC_CommonInitTypeDef ADC_CommonInitStruct;
     DMA_InitTypeDef DMA_InitStructure;    
     GPIO_InitTypeDef      GPIO_InitStructure;
 
@@ -22,33 +24,32 @@ int main(){
     GPIO_Init(GPIOA, &GPIO_InitStructure);
     
     // ADC_CommonStructInit
-    ADC_CommonInitStruct->ADC_Mode = ADC_Mode_Independent;
-    ADC_CommonInitStruct->ADC_Prescaler = ADC_Prescaler_Div2;
-    ADC_CommonInitStruct->ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
-    ADC_CommonInitStruct->ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
-    ADC_CommonInit(&ADC_CommonInitStructure);
+    ADC_CommonInitStruct.ADC_Mode = ADC_Mode_Independent;
+    ADC_CommonInitStruct.ADC_Prescaler = ADC_Prescaler_Div2;
+    ADC_CommonInitStruct.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+    ADC_CommonInitStruct.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+    ADC_CommonInit(&ADC_CommonInitStruct);
     
     // ADC_StructInit 
-    ADC_InitStruct->ADC_Resolution = ADC_Resolution_12b;
-    ADC_InitStruct->ADC_ScanConvMode = DISABLE;
-    ADC_InitStruct->ADC_ContinuousConvMode = DISABLE;
-    ADC_InitStruct->ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-    ADC_InitStruct->ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
-    ADC_InitStruct->ADC_DataAlign = ADC_DataAlign_Right;
-    ADC_InitStruct->ADC_NbrOfConversion = 1;
-    ADC_InitStructure.ADC_Resolution = ADC_Resolution_8b;
-    ADC_Init(ADC1, &ADC_InitStructure);
+    ADC_InitStruct.ADC_ScanConvMode = DISABLE;
+    ADC_InitStruct.ADC_ContinuousConvMode = ENABLE;
+    ADC_InitStruct.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+    ADC_InitStruct.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
+    ADC_InitStruct.ADC_DataAlign = ADC_DataAlign_Right;
+    ADC_InitStruct.ADC_NbrOfConversion = 1;
+    ADC_InitStruct.ADC_Resolution = ADC_Resolution_8b;
+    ADC_Init(ADC1, &ADC_InitStruct);
     
     ADC_RegularChannelConfig(ADC1, ADC_Channel_0, 1, ADC_SampleTime_3Cycles);
     ADC_DMARequestAfterLastTransferCmd(ADC1, ENABLE);
 
     DMA_InitStructure.DMA_Channel = DMA_Channel_0; 
     DMA_InitStructure.DMA_PeripheralBaseAddr = ADC1_DR_ADDRESS;
-    DMA_InitStructure.DMA_Memory0BaseAddr =(unsigned int) &value_s;
+    DMA_InitStructure.DMA_Memory0BaseAddr =(unsigned int) &value;
     DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralToMemory;
-    DMA_InitStructure.DMA_BufferSize = 1;
+    DMA_InitStructure.DMA_BufferSize = 1024;
     DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
     DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
     DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
     DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
@@ -64,6 +65,82 @@ int main(){
 
     ADC_DMACmd(ADC1, ENABLE);
     ADC_SoftwareStartConv(ADC1);
+
+    /* DMA1 clock and GPIOA clock enable (to be used with DAC) */
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1 | RCC_AHB1Periph_GPIOA, ENABLE);
+ 
+    /* DAC Periph clock enable */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC, ENABLE);
+
+    /* DAC channel 1 & 2 (DAC_OUT1 = PA.4)(DAC_OUT2 = PA.5) configuration */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 | GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* TIM6 Configuration ------------------------------------------------------*/
+    /* TIM6CLK = HCLK / 4 = SystemCoreClock /4 */
+  
+    TIM_TimeBaseInitTypeDef    TIM_TimeBaseStructure;
+    /* TIM6 Periph clock enable */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6, ENABLE);
+
+    /* Time base configuration */
+    TIM_TimeBaseStructInit(&TIM_TimeBaseStructure);
+    TIM_TimeBaseStructure.TIM_Period = 0x01;
+    TIM_TimeBaseStructure.TIM_Prescaler = 10;
+    TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; 
+    TIM_TimeBaseInit(TIM6, &TIM_TimeBaseStructure);
+
+    /* TIM6 TRGO selection */
+    TIM_SelectOutputTrigger(TIM6, TIM_TRGOSource_Update);
+  
+    /* TIM6 enable counter */
+    TIM_Cmd(TIM6, ENABLE);
+
+    DAC_InitTypeDef  DAC_InitStructure;
+  
+    /* DAC channel2 Configuration */
+    DAC_InitStructure.DAC_Trigger = DAC_Trigger_T6_TRGO;
+    DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
+    DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
+    DAC_Init(DAC_Channel_2, &DAC_InitStructure);
+    DAC_InitStructure.DAC_Trigger = DAC_Trigger_None;
+    DAC_InitStructure.DAC_WaveGeneration = DAC_WaveGeneration_None;
+    DAC_InitStructure.DAC_OutputBuffer = DAC_OutputBuffer_Enable;
+    DAC_Init(DAC_Channel_1, &DAC_InitStructure);
+
+    /* DMA1_Stream5 channel7 configuration **************************************/
+    DMA_DeInit(DMA1_Stream5);
+    DMA_InitStructure.DMA_Channel = DMA_Channel_7;  
+    DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t) 0x4000741C;
+    DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t) &value;
+    DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+    DMA_InitStructure.DMA_BufferSize = 1024;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+    DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+    DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+    DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull;
+    DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single;
+    DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single;
+    DMA_Init(DMA1_Stream6, &DMA_InitStructure);
+
+    /* Enable DMA1_Stream5 */
+    DMA_Cmd(DMA1_Stream6, ENABLE);
+
+    /* Enable DAC Channel2 */
+    DAC_Cmd(DAC_Channel_2, ENABLE);
+    DAC_Cmd(DAC_Channel_1, ENABLE);
+    DAC_SetChannel1Data(DAC_Align_8b_R, value[0]);
+
+    /* Enable DMA for DAC Channel2 */
+    DAC_DMACmd(DAC_Channel_2, ENABLE);
+
 
     
 
